dbd19c5d07ec11a6ae817313d59a7c5087f59acb
Maryiam Khan <168927125+maryamkhn24@users.noreply.github.com>
2025-04-11 20:00:08 +0500

    Update thread_handler.c

--- code/thread_handler.c
+++ code/thread_handler.c
@@ -1,81 +1,57 @@
-// need to modify this for proper implementation of multithreading (threadpool)
-
-#include <stdio.h>
-#include <stdlib.h>
+#include "communication.h"
 #include <pthread.h>
-#include <unistd.h>
-#include <string.h>
-#include <signal.h>
-#include "thread_handler.h"
 
-#define THREAD_COUNT 3
+#define NUM_THREADS 3
 
-static pthread_t threads[THREAD_COUNT];
-static JobQueue *sharedQueue;
-static int semid_global;
-static int running = 1;
+extern JobQueue *queue;
+extern int semid;
 
-void sem_wait_custom(int sem_num) 
-{
-    struct sembuf sb={sem_num, -1, 0};
-    semop(semid_global, &sb, 1);
+/*void sem_wait(int sem_num) {
+    struct sembuf op = {sem_num, -1, 0};
+    semop(semid, &op, 1);
 }
 
-void sem_signal_custom(int sem_num) 
-{
-    struct sembuf sb={sem_num, 1, 0};
-    semop(semid_global, &sb, 1);
-}
+void sem_signal(int sem_num) {
+    struct sembuf op = {sem_num, 1, 0};
+    semop(semid, &op, 1);
+}*/
 
-void* thread_function(void* arg) 
-{
-    while (running) 
-    {
-        sem_wait_custom(FULL);
-        sem_wait_custom(MUTEX);
+void* worker_thread(void* arg) {
+    int thread_id = *(int*)arg;
 
-        Job job = sharedQueue->jobs[sharedQueue->front];
-        sharedQueue->front = (sharedQueue->front + 1) % MAX_JOBS;
-        sharedQueue->count--;
+    while (1) {
+        sem_wait(FULL);
+        sem_wait(MUTEX);
 
-        sem_signal_custom(MUTEX);
-        sem_signal_custom(EMPTY);
+        Job job = queue->jobs[queue->front];
+        queue->front = (queue->front + 1) % MAX_JOBS;
+        queue->count--;
 
-        if (strcmp(job.content, "exit") == 0) 
-        {
-            printf("[THREAD %ld]: Exit job received.\n", pthread_self());
-            running = 0;
+        sem_signal(MUTEX);
+        sem_signal(EMPTY);
+
+        if (strcmp(job.content, "exit") == 0) {
+            printf("[THREAD %d] Received exit signal. Exiting...\n", thread_id);
             break;
         }
 
-        printf("[THREAD %ld]: Processing job ID %d | Content: %s\n", pthread_self(), job.jobid, job.content);
-        sleep(2); 
-        printf("[THREAD %ld]: Finished job ID %d\n", pthread_self(), job.jobid);
+        sleep(1); // simulate processing
+        printf("[THREAD %d] Processing Job ID = %d | Content: %s\n", thread_id, job.jobid, job.content);
     }
 
     return NULL;
 }
 
-void start_thread_pool(JobQueue *queue, int semid) 
-{
-    sharedQueue = queue;
-    semid_global = semid;
-    running = 1;
+void start_thread_pool() {
+    pthread_t threads[NUM_THREADS];
+    int thread_ids[NUM_THREADS];
 
-    for (int i = 0; i < THREAD_COUNT; ++i)
-    {
-        pthread_create(&threads[i], NULL, thread_function, NULL);
+    for (int i = 0; i < NUM_THREADS; i++) {
+        thread_ids[i] = i + 1;
+        pthread_create(&threads[i], NULL, worker_thread, &thread_ids[i]);
     }
-    printf("[THREAD_HANDLER]: Started %d threads.\n", THREAD_COUNT);
-}
 
-void stop_thread_pool()
-{
-    for (int i = 0; i < THREAD_COUNT; ++i) 
-      {   
+    for (int i = 0; i < NUM_THREADS; i++) {
         pthread_join(threads[i], NULL);
     }
-
-    printf("[THREAD_HANDLER]: All threads exited.\n");
 }
-
