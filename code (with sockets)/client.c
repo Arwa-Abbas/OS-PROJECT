eaf22b74e97e5c8426ae4554ed009b5fefab44b8
Arwa Abbas <arwaabbaskerani@gmail.com>
2025-04-17 21:44:43 +0500

    Update client.c

--- code (with sockets)/client.c
+++ code (with sockets)/client.c
@@ -8,101 +8,130 @@
 
 #define TEXT_LIMIT 512
 
-int main() 
+int main()
 {
     int sock=0;
     struct sockaddr_in serv_addr;
-    if ((sock=socket(AF_INET,SOCK_STREAM,0)) < 0) 
+    if ((sock=socket(AF_INET,SOCK_STREAM,0)) < 0)
     {
         printf("\n Socket Creation Error \n");
         return -1;
     }
-    
+   
     serv_addr.sin_family=AF_INET;
     serv_addr.sin_port=htons(PORT);
-    
+   
     char server_ip[16];
     printf("\nEnter Server IP Address to Connect: ");
     scanf("%15s",server_ip);
     getchar();
-    
-    if (inet_pton(AF_INET, server_ip, &serv_addr.sin_addr) <= 0)   //this takes ip address
+   
+    if (inet_pton(AF_INET, server_ip, &serv_addr.sin_addr) <= 0)
     {
         printf("\nInvalid address/ Address not supported \n");
         return -1;
     }
-    
-    if (connect(sock,(struct sockaddr *)&serv_addr,sizeof(serv_addr))<0) 
+   
+    if (connect(sock,(struct sockaddr *)&serv_addr,sizeof(serv_addr))<0)
     {
         printf("\nConnection Failed \n");
         return -1;
     }
-    
+   
     int pid=getpid();
     printf("[CLIENT %d] Connected to Server at %s:%d\n",pid,server_ip,PORT);
     printf("\n------------WELCOME [CLIENT %d]---------------\n",pid);
-    
+   
     char jobtext[TEXT_LIMIT];
     struct message msg;
     msg.mestype=1;
     int choice;
 
-    while (1) 
+    while (1)
     {
         printf("\nDo you want to:\n");
         printf("1. Add a Printing Job\n");
         printf("2. Exit\n");
         printf("Enter your choice: ");
         scanf("%d", &choice);
-        getchar(); 
-        
-        if (choice==2) 
+        getchar();
+       
+        if (choice==2)
         {
             printf("Exiting...\n");
             break;
         }
-        else if (choice==1) 
+        else if (choice==1)
         {
-            printf("\n------------------------------------------------------------\n");
-            printf("Enter 'exit' if you want to discontinue");
-            printf("\n------------------------------------------------------------\n\n");       
-            char jobtext[TEXT_LIMIT] = "";  
-            char filename[TEXT_LIMIT], heading[TEXT_LIMIT], content[TEXT_LIMIT];
-            
-            printf("[CLIENT %d] Filename: ", pid);
-            fgets(filename, sizeof(filename), stdin);
-            filename[strcspn(filename, "\n")] = '\0';
-            if (strcmp(filename, "exit") == 0)
-            break;
+             int job_type;
+             printf("\n-----------Choose Job Type--------------\n\n");
+             printf("1. Submit Existing File (server will read the file)\n");
+             printf("2. Create a New file (you provide heading and content)\n");
+             printf("Enter your choice: ");
+             scanf("%d", &job_type);
+             getchar();
+             
+            if (job_type==1)
+            {
+                printf("\n------------------------------------------------------------\n");
+                char filename[TEXT_LIMIT];
+                printf("[CLIENT %d] filename: ",pid);
+                fgets(filename, sizeof(filename), stdin);
+                filename[strcspn(filename, "\n")] = '\0';
+                strncpy(msg.mesfilename, filename, TEXT_LIMIT);
+                msg.job_type = 1;  
+               
+                send(sock, &msg, sizeof(msg), 0);
+                printf("[CLIENT %d] Sent request to print existing file: %s\n", pid, filename);
+
+                char buffer[MSG_SIZE] = {0};
+                read(sock, buffer, MSG_SIZE);
+                printf("[CLIENT %d] Server Response: %s\n", pid, buffer);
+            }
            
-            printf("\n---------Now Enter Content of File---------\n");
-            printf("\nHeading: ");
-            fgets(heading, sizeof(heading), stdin);
-            heading[strcspn(heading, "\n")] = '\0';
-            if (strcmp(heading, "exit") == 0)
-            break;
+            else if (job_type==2)
+            {
+                printf("\n------------------------------------------------------------\n");
+                printf("Enter 'exit' if you want to discontinue");
+                printf("\n------------------------------------------------------------\n\n");      
+                char filename[TEXT_LIMIT], heading[TEXT_LIMIT], content[TEXT_LIMIT];
+               
+                printf("[CLIENT %d] Filename: ", pid);
+                fgets(filename, sizeof(filename), stdin);
+                filename[strcspn(filename, "\n")] = '\0';
+                if (strcmp(filename, "exit") == 0)
+                    break;
+               
+                printf("\n---------Now Enter Content of File---------\n");
+                printf("\nHeading: ");
+                fgets(heading, sizeof(heading), stdin);
+                heading[strcspn(heading, "\n")] = '\0';
+                if (strcmp(heading, "exit") == 0)
+                    break;
 
-            printf("Content: ");
-            fgets(content, sizeof(content), stdin);
-            content[strcspn(content, "\n")] = '\0';
-           if (strcmp(content, "exit") == 0)
-           break;
+                printf("Content: ");
+                fgets(content, sizeof(content), stdin);
+                content[strcspn(content, "\n")] = '\0';
+                if (strcmp(content, "exit") == 0)
+                    break;
 
-           strncpy(msg.mesfilename, filename, TEXT_LIMIT);
-           strncpy(msg.mesheading, heading, TEXT_LIMIT);
-           strncpy(msg.mescontent, content, TEXT_LIMIT);
-           send(sock, &msg, sizeof(msg), 0);
-           printf("[CLIENT %d] Sent File: %s | Heading: %s | Content: %s\n", pid, msg.mesfilename,msg.mesheading,msg.mescontent);
+                msg.job_type = 2;  // Explicitly set job type to 2
+                strncpy(msg.mesfilename, filename, TEXT_LIMIT);
+                strncpy(msg.mesheading, heading, TEXT_LIMIT);
+                strncpy(msg.mescontent, content, TEXT_LIMIT);
+                send(sock, &msg, sizeof(msg), 0);
+                printf("[CLIENT %d] Sent File: %s | Heading: %s | Content: %s\n", pid, msg.mesfilename,msg.mesheading,msg.mescontent);
 
-           char buffer[MSG_SIZE] = {0};
-           read(sock, buffer, MSG_SIZE);
-           printf("[CLIENT %d] Server Response: %s\n", pid, buffer);
-         }
-        else 
+                char buffer[MSG_SIZE] = {0};
+                read(sock, buffer, MSG_SIZE);
+                printf("[CLIENT %d] Server Response: %s\n", pid, buffer);
+            }
+        }
+        else
         {
             printf("Invalid option. Please try again.\n");
         }
-    }   
+    }  
     close(sock);
     return 0;
 }
